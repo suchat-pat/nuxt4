a53cd22b597c7496973551a5dd9a7cbfe2e961f7
peet5228 <ppeet5228@gmail.com>
2026-01-19 15:17:36 +0700

    updae

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-    <v-row></v-row>
+    <v-container class="fill-height">
+        <v-row align="center" justify="center">
+            <v-col cols="12" md="6" lg="4">
+                <v-card color="#7d0c14" elevation="10" rounded="lg">
+                    <p class="text-center text-white text-h5 font-weight-bold">NTC EVALUATION SYSTEM</p>
+                    <p class="text-center text-white font-weight-bold">ระบบประเมินบุคลากรวิทยาลัยเทคนิคน่าน</p>
+                    <v-container class="bg-white">
+                        <p class="text-center font-weight-bold">เข้าสู่ระบบ</p>
+                        <v-alert valaint="tona;"></v-alert>
+                    </v-container>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup lang="ts">
